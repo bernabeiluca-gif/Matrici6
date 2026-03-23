b7fb4472b792a1b170e33f9dd1057231947bc9ef
bernabei.luca <bernabei.luca@INF-1008>
2026-03-23 08:59:17 +0100

    Aggiungere i file di progetto.

---
+++ Matrici6/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Matrici6
+{
+    internal class Program
+    {
+
+        static int creare(int[,] M)
+        {
+
+            int c = 90;
+            int c1 = 10;
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                for(int j=0;  j < M.GetLength(1); j++)
+                {
+                    
+
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            int[,] M = new int[10, 10];
+
+        }
+    }
+}
